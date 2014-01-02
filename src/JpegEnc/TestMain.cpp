5d22eb5bd7c392e3776ae4b6e353a491b097c420
Matias Juntunen <matias.juntunen@gmail.com>
2014-01-02 15:58:19 +0200

    Test reorganization.

---
+++ src/JpegEnc/TestMain.cpp
@@ -0,0 +1,5 @@
+#include "stdafx.h"
+
+// This is needed to avoid multiple definitions of the test suite initialization functions.
+#define BOOST_TEST_MODULE Tests
+#include <boost/test/unit_test.hpp>
