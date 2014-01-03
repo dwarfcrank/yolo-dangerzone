0ff7663c2ddba2c54e0054947eef0ec41a0a7dc8
Matias Juntunen <matias.juntunen@gmail.com>
2014-01-03 17:21:06 +0200

    Added a matrix multiplication utility function.

---
+++ src/JpegEnc/MatrixTest.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "block.h"
+#include "matrix.h"
+
+#include <vector>
+#include <cstdint>
+
+#include <boost/test/unit_test.hpp>
+
+const Int16Block TEST_MATRIX_A =
+{
+    1, 0, 0, 0, 0, 0, 0, 2,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 3, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+const Int16Block TEST_MATRIX_B =
+{
+    1, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 2, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0,
+};
+
+const Int16Block EXPECTED =
+{
+    7, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 6, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+BOOST_AUTO_TEST_CASE(Int16MultiplyTest)
+{
+    Int16Block result;
+
+    Util::MatrixMultiply(&result, TEST_MATRIX_A, TEST_MATRIX_B);
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(result.cbegin(), result.cend(),
+        EXPECTED.cbegin(), EXPECTED.cend());
+}
---
+++ src/JpegEnc/matrix.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "block.h"
+
+namespace Util
+{
+
+/**
+Performs 8x8 matrix multiplication such that Result = MatrixA * MatrixB.
+Each matrix must be a type based on std::array with 64 elements.
+
+@param[out] Result  The resulting matrix.
+@param[in]  MatrixA The matrix on the left side of the multiplication.
+@param[in]  MatrixB The matrix on the right side of the multiplication.
+*/
+template<typename TSrcMatrix1, typename TSrcMatrix2, typename TDestMatrix>
+void MatrixMultiply(TDestMatrix* Result, const TSrcMatrix1& MatrixA, const TSrcMatrix2& MatrixB)
+{
+    typedef TDestMatrix::value_type ResultType;
+
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            ResultType& value = Result->at(y * 8 + x);
+            value = 0;
+
+            for (int i = 0; i < 8; i++) {
+                value += static_cast<ResultType>(MatrixA[y * 8 + i] * MatrixB[i * 8 + x]);
+            }
+        }
+    }
+}
+
+}
