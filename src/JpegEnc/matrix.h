8f72c36246a4ba42a7a810dda7e593e979271068
Matias Juntunen <matias.juntunen@gmail.com>
2014-01-06 00:59:59 +0200

    Improved enforcing of type restrictions.

--- src/JpegEnc/matrix.h
+++ src/JpegEnc/matrix.h
@@ -7,24 +7,22 @@ namespace Util
 
 /**
 Performs 8x8 matrix multiplication such that Result = MatrixA * MatrixB.
-Each matrix must be a type based on std::array with 64 elements.
 
 @param[out] Result  The resulting matrix.
 @param[in]  MatrixA The matrix on the left side of the multiplication.
 @param[in]  MatrixB The matrix on the right side of the multiplication.
 */
-template<typename TSrcMatrix1, typename TSrcMatrix2, typename TDestMatrix>
-void MatrixMultiply(TDestMatrix* Result, const TSrcMatrix1& MatrixA, const TSrcMatrix2& MatrixB)
+template<typename TSrcValue1, typename TSrcValue2, typename TDestValue>
+void MatrixMultiply(std::array<TDestValue, 64>* Result, const std::array<TSrcValue1, 64>& MatrixA,
+                    const std::array<TSrcValue2, 64>& MatrixB)
 {
-    typedef TDestMatrix::value_type ResultType;
-
     for (int y = 0; y < 8; y++) {
         for (int x = 0; x < 8; x++) {
-            ResultType& value = Result->at(y * 8 + x);
+            TDestValue& value = Result->at(y * 8 + x);
             value = 0;
 
             for (int i = 0; i < 8; i++) {
-                value += static_cast<ResultType>(MatrixA[y * 8 + i] * MatrixB[i * 8 + x]);
+                value += static_cast<TDestValue>(MatrixA[y * 8 + i] * MatrixB[i * 8 + x]);
             }
         }
     }
@@ -32,14 +30,13 @@ void MatrixMultiply(TDestMatrix* Result, const TSrcMatrix1& MatrixA, const TSrcM
 
 /**
 Computes the transpose of a matrix which will replace the matrix passed to the function.
-The matrix must be a type based on std::array with 64 elements.
 
 @param[in,out]  Matrix  The matrix to transpose.
 */
-template<typename TMatrix>
-void MatrixTranspose(TMatrix* Matrix)
+template<typename TValue>
+void MatrixTranspose(std::array<TValue, 64>* Matrix)
 {
-    TMatrix transpose;
+    std::array<TValue, 64> transpose;
 
     for (int y = 0; y < 8; y++) {
         for (int x = 0; x < 8; x++) {
