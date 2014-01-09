11c93c9dd266abac85657d12d0c3f84146572334
Matias Juntunen <matias.juntunen@gmail.com>
2014-01-09 23:42:41 +0200

    Refactored RGB->YCbCr conversion to perform in-place.

--- src/JpegEnc/colorutil.cpp
+++ src/JpegEnc/colorutil.cpp
@@ -4,29 +4,23 @@
 namespace Util
 {
 
-std::vector<uint8_t> ConvertRGBToYCbCr(const std::vector<std::uint8_t>& Pixels)
+void ConvertRGBToYCbCr(std::vector<std::uint8_t>* Pixels)
 {
-    assert(Pixels.size() % 3 == 0);
+    assert(Pixels->size() % 3 == 0);
 
-    std::vector<uint8_t> result;
-    result.reserve(Pixels.size());
-
-    auto end = Pixels.cend();
-    for (auto it = Pixels.cbegin(); it != end;) {
-        std::uint8_t b = *it++;
-        std::uint8_t g = *it++;
-        std::uint8_t r = *it++;
+    for (auto it = Pixels->begin(); it != Pixels->end(); it += 3) {
+        std::uint8_t b = *it;
+        std::uint8_t g = *(it + 1);
+        std::uint8_t r = *(it + 2);
 
         auto y  = static_cast<std::uint8_t>(  0 +    0.299f * r +    0.587f * g +    0.114f * b);
         auto cb = static_cast<std::uint8_t>(128 - 0.168736f * r - 0.331264f * g +      0.5f * b);
         auto cr = static_cast<std::uint8_t>(128 +      0.5f * r - 0.418688f * g - 0.081312f * b);
 
-        result.push_back(y);
-        result.push_back(cb);
-        result.push_back(cr);
+        *it = y;
+        *(it + 1) = cb;
+        *(it + 2) = cr;
     }
-
-    return result;
 }
 
 void SplitChannels(const std::vector<std::uint8_t>& Pixels, std::vector<std::uint8_t>* Channel1,
--- src/JpegEnc/colorutil.h
+++ src/JpegEnc/colorutil.h
@@ -7,15 +7,14 @@ namespace Util
 {
 
 /**
-Performs color space transformation from RGB to YCbCr.
+Performs in-place color space transformation from RGB to YCbCr.
 
 The pixel data is assumed to be stored as tightly packed 24-bpp RGB. The formula
 used for pixel conversion is taken from http://en.wikipedia.org/wiki/YCbCr#JPEG_conversion.
 
-@param[in] Pixels   An array of 24-bpp RGB pixels. The size of the array must be a multiple of 3.
-@return             The pixel data converted to YCbCr or an empty array in case of incomplete data. 
+@param[in,out]  Pixels  An array of 24-bpp RGB pixels. The size of the array must be a multiple of 3.
 */
-std::vector<uint8_t> ConvertRGBToYCbCr(const std::vector<std::uint8_t>& Pixels);
+void ConvertRGBToYCbCr(std::vector<std::uint8_t>* Pixels);
 
 /**
 Splits packed 24bpp pixels into three separate arrays, each containing only a single channel.
