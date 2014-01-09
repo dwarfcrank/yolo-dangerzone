9dced890efed4bb4b92a6206330e9f0dca74031f
Matias Juntunen <matias.juntunen@gmail.com>
2014-01-09 21:17:13 +0200

    Fixed RGB to YCbCr conversion.

--- src/JpegEnc/colorutil.cpp
+++ src/JpegEnc/colorutil.cpp
@@ -8,13 +8,14 @@ std::vector<uint8_t> ConvertRGBToYCbCr(const std::vector<std::uint8_t>& Pixels)
 {
     assert(Pixels.size() % 3 == 0);
 
-    std::vector<uint8_t> result(Pixels.size());
+    std::vector<uint8_t> result;
+    result.reserve(Pixels.size());
 
     auto end = Pixels.cend();
     for (auto it = Pixels.cbegin(); it != end;) {
-        std::uint8_t r = *it++;
-        std::uint8_t g = *it++;
         std::uint8_t b = *it++;
+        std::uint8_t g = *it++;
+        std::uint8_t r = *it++;
 
         auto y  = static_cast<std::uint8_t>(  0 +    0.299f * r +    0.587f * g +    0.114f * b);
         auto cb = static_cast<std::uint8_t>(128 - 0.168736f * r - 0.331264f * g +      0.5f * b);
